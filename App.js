9b83e0bef271fd7bf119e8391a1c2ea7f7cad6c1
Aditya Pratama Badra <orochimadit@gmail.com>
2019-09-12 09:30:03 +0700

    layout flex

--- App.js
+++ App.js
@@ -27,28 +27,33 @@ export default class App extends Component{
   }
   render(){
     return(
-      <View>
-        <StatusBar backgroundColor="blue"/>
-        <Text style={styles.header}>First APp</Text>
-        <Header person="Nama : Badra"/>
+      // <View>
+      //   <StatusBar backgroundColor="blue"/>
+      //   <Text style={styles.header}>First APp</Text>
+      //   <Header person="Nama : Badra"/>
         
-        <Footer tahun="2019"/>
-        <Button title="Button tambah" onPress={this.handleTambah} />
-        <Text style={styles.jumlah}>Jumlah : {this.state.jumlah}</Text>
+      //   <Footer tahun="2019"/>
+      //   <Button title="Button tambah" onPress={this.handleTambah} />
+      //   <Text style={styles.jumlah}>Jumlah : {this.state.jumlah}</Text>
+      // </View>
+      <View style={{flex:1,backgroundColor:"red"}}>
+        <Text style={{flex:4,backgroundColor:"yellow",margin:20}}>Halaman 1</Text>
+        <Text style={{flex:1,backgroundColor:"green"}} >Halaman 2</Text>
       </View>
     );
   }
 }
-const styles =StyleSheet.create({
-  header:{
-    marginTop:20,
-    fontSize:20,
-    textAlign:"center"
-  },
-  jumlah:{
-    fontSize:20,
-    marginTop:30,
-    textAlign:"center",
-    color:"red"
-  }
-})
+
+// const styles =StyleSheet.create({
+//   header:{
+//     marginTop:20,
+//     fontSize:20,
+//     textAlign:"center"
+//   },
+//   jumlah:{
+//     fontSize:20,
+//     marginTop:30,
+//     textAlign:"center",
+//     color:"red"
+//   }
+// })
