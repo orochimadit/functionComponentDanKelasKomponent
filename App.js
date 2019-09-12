0ee5291b79fe8b056a171a0dcd32ca0ea6152181
Aditya Pratama Badra <orochimadit@gmail.com>
2019-09-12 07:59:56 +0700

    belajar handling touch

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React , {Component } from 'react';
-import {Platform, StyleSheet,Text,View,StatusBar} from "react-native";
+import {Button, Alert,Text,View,StatusBar} from "react-native";
 import Header from "./Header";
 import Footer from "./Footer"
 //belajar function component 
 
 
 export default class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      jumlah:0
+    };
+  }
+  handleTambah=()=>{
+   // Alert.alert("Ini adalah Handle Tambah");
+   this.setState({
+    jumlah:this.state.jumlah+1
+   })
+   
+  }
   render(){
     return(
       <View>
@@ -13,6 +26,8 @@ export default class App extends Component{
         <Header person="Nama : Badra"/>
         <Text>First APp</Text>
         <Footer tahun="2019"/>
+        <Button title="Button tambah" onPress={this.handleTambah} />
+        <Text>Jumlah : {this.state.jumlah}</Text>
       </View>
     );
   }
